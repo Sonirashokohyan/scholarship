8088d9bc741bb6bb139be47550dd8469c2c673c2
sonira <sonira.shokohyan531@gmail.com>
2023-11-09 23:39:35 +0500

    new

--- app/views.py
+++ app/views.py
@@ -27,7 +27,8 @@ Message: {message1}
 Name: {name}
 Phone: {phone}
 Email:{email}"""
-        recipient = 'rahmatullah.kanjo123@gmail.com'
+        recipient = 'sonira.shokoyan123@gmail.com'
+        # recipient = 'rahmatullah.kanjo123@gmail.com'
 
         # Create the email message
         email = EmailMessage()
